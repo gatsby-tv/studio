bf89d144afcf9c19eb4b237cf2b7172e6a7bb95a
Michael Nesbitt <nesbitt@treeman.nesbitt.rocks>
2021-06-12 00:53:58 -0500

    Expanded script to downloading dapper release

--- scripts/package.windows.js
+++ scripts/package.windows.js
@@ -1,5 +1,42 @@
 const { silent, run } = require('./index');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
 
-silent('rm -rf dist');
-run('yarn build');
-run('electron-builder build -w --publish never');
+var dapperDir = "./dapper";
+var file = fs.createWriteStream(path.join(dapperDir, "dapper.exe"));
+
+function downloadDapper() {
+	return new Promise((resolve, reject) => {
+    https.get("https://github.com/gatsby-tv/dapper/releases/download/v0.1.1/dapper.exe", (response) => {
+      response.pipe(file);
+      file.on('finish', function() {
+        file.close();
+      });
+
+      response.on('end', () => {
+        resolve();
+      });
+
+      response.on('error', (error) => {
+        reject(error);
+      });
+    });
+  });
+}
+
+(async function () {
+  try {
+    if (!fs.existsSync(dapperDir)){
+      fs.mkdirSync(dapperDir);
+    }
+    await downloadDapper();
+
+    silent('rm -rf dist');
+    run('yarn build');
+    run('electron-builder build -w --publish never');
+  }
+  catch(error) {
+    console.log(error);
+  }
+})();
