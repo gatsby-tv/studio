77ad0054821837a96d46fa658957623ddcf7bdc5
Michael Nesbitt <nesbitt@treeman.nesbitt.rocks>
2021-06-12 12:27:56 -0500

    I have no idea what I'm doing

--- scripts/package.windows.js
+++ scripts/package.windows.js
@@ -1,28 +1,17 @@
 const { silent, run } = require('./index');
-const https = require('https');
-const fs = require('fs');
+const fetch = require('node-fetch');
+const { pipeline }  = require('stream');
+const { promisify } = require('util');
+const { createWriteStream } = require('fs');
 const path = require('path');
 
 var dapperDir = "./dapper";
-var file = fs.createWriteStream(path.join(dapperDir, "dapper.exe"));
 
-function downloadDapper() {
-	return new Promise((resolve, reject) => {
-    https.get("https://github.com/gatsby-tv/dapper/releases/download/v0.1.1/dapper.exe", (response) => {
-      response.pipe(file);
-      file.on('finish', function() {
-        file.close();
-      });
-
-      response.on('end', () => {
-        resolve();
-      });
-
-      response.on('error', (error) => {
-        reject(error);
-      });
-    });
-  });
+async function downloadFile(url, file) {
+  const streamPipeline = promisify(pipeline);
+  const response = await fetch(url);
+  if (!response.ok) throw new Error(`unexpected response ${response.statusText}`);
+  await streamPipeline(response.body, file);
 }
 
 (async function () {
@@ -30,7 +19,13 @@ function downloadDapper() {
     if (!fs.existsSync(dapperDir)){
       fs.mkdirSync(dapperDir);
     }
-    await downloadDapper();
+
+    var dapperExe = createWriteStream(path.join(dapperDir, "dapper.exe"));
+    await downloadFile("https://github.com/gatsby-tv/dapper/releases/download/v0.1.1/dapper.exe", dapperExe);
+    var ffmpeg7z = createWriteStream(path.join(dapperDir, "ffmpeg.7z"));
+    await downloadFile("https://www.gyan.dev/ffmpeg/builds/packages/ffmpeg-2021-06-09-git-e01bf559df-essentials_build.7z", ffmpeg7z);
+    silent('7z e -o' + path.join(dapperDir, "ffmpeg") + ' ' + path.join(dapperDir, "ffmpeg.7z"));
+    silent('rm -f ' + path.join(dapperDir, "ffmpeg.7z"));
 
     silent('rm -rf dist');
     run('yarn build');
