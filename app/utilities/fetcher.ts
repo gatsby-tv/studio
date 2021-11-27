8a93f29c97fc2efbdee5590d3cce4d02c00809c3
cjayross <calvinjayross@gmail.com>
2021-11-27 11:17:33 -0600

    added dev-mode fetcher

--- app/utilities/fetcher.ts
+++ app/utilities/fetcher.ts
@@ -1,6 +1,13 @@
 import { Fetcher } from '@gatsby-tv/utilities';
 
-export const fetcher = Fetcher(
-  process.env.WESTEGG_URL,
-  process.env.WESTEGG_VERSION
-);
+export const fetcher =
+  process.env.NODE_ENV !== 'development'
+    ? Fetcher(process.env.WESTEGG_URL, process.env.WESTEGG_VERSION)
+    : (endpoint: string) => {
+        const url = new URL(
+          '/dist'.concat(endpoint).concat('.json'),
+          'http://localhost:1212'
+        );
+
+        return fetch(url.toString());
+      };
--- scripts/start.js
+++ scripts/start.js
@@ -1,4 +1,5 @@
 const { run } = require('./index');
 
+run('yarn run copyfiles "./public/**/*" "./dist" --up=1');
 run('yarn run cross-env NODE_ENV=development webpack --config configs/webpack.config.server.dev.js');
 run('yarn run cross-env NODE_ENV=development webpack serve --config configs/webpack.config.renderer.dev.js');
